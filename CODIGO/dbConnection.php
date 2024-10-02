57cb207f5d1879a2ce268bcee5c879a1d574e0ce
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-10-02 11:12:14 -0500

    Update dbConnection.php

--- CODIGO/dbConnection.php
+++ CODIGO/dbConnection.php
@@ -1,8 +1,8 @@
 <?php
 $databaseHost = 'localhost';
 $databaseName = 'CRUD';
-$databaseUsername = 'root';
-$databasePassword = '';
+$databaseUsername = 'seu_usuario';
+$databasePassword = 'sua_senha';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
